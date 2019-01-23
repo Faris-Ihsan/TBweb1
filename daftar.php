b087c162c44939cba6b3c067c40f8813531c1f10
Faris Muhammad Ihsan <44759002+Faris-Ihsan@users.noreply.github.com>
2019-01-23 10:30:33 +0700

    design

---
+++ daftar.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Pendaftaran</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="w3.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="btn.css" />
+</head>
+
+<body>
+    <div class="main w3-display-container">
+        <div class="w3-container">
+            <div class="daftar1 w3-display-topleft w3-card-4">
+                <div class="">
+                    <div class="loginbutton w3-container">
+                        <h5><b>Pendaftaran</b></h5>
+                    </div>
+                    <form class="w3-container">
+                        <p>
+                            <label><b>Nama Lengkap</b></label>
+                            <input class="w3-input w3-border" type="text" name="nama" style="width: ;">
+                        </p>
+                        <p>
+                            <label><b>Jurusan</b></label>
+                            <input class="w3-input w3-border" type="text" name="jurusan" style="width: ;">
+                        </p>
+                        <p>
+                            <label><b>npm</b></label>
+                            <input class="w3-input w3-border" type="text" name="npm" style="width: ;">
+                        </p>
+                        <p>
+                            <label><b>Email</b></label>
+                            <input class="w3-input w3-border" type="text" name="" style="width: ;">
+                        </p>
+
+                        <div class="wrapper">
+                            <div class="btn">
+                                <button type="button"><b>Daftar</b></button>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+
+<style>
+    .daftar1 {
+        width: 95%;
+        height: 450px;
+        background: #f0e58f;
+        margin: 40px;
+    }
+</style>
---
+++ login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Halaman Login</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="w3.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="btn.css" />
+</head>
+
+<body>
+    <div class="main w3-display-container">
+        <div class="w3-container">
+            <div class="login1 w3-display-middle">
+                <div class="form w3-card-4">
+                    <div class="loginbutton w3-container">
+                        <h5>Login</h5>
+                    </div>
+                    <form class="w3-container">
+                        <p>
+                            <label><b>Username</b></label>
+                            <input class="w3-input w3-border" type="text" name="uname" maxlength="30" style="width: 350px;">
+                        </p>
+                        <p>
+                            <label><b>Password</b></label>
+                            <input class="w3-input w3-border" type="password" name="password" maxlength="30" style="width: 350px">
+                        </p>
+                        <div class="wrapper">
+                            <div class="btn">
+                                <button type="button"><b>Login</b></button>
+                            </div>
+                        </div>
+                        <p>
+                            <font size="2">
+                                Belum punya akun? <a href="daftar.html">
+                                    <font color="blue">Daftar Sekarang!</font>
+                                </a></font>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
